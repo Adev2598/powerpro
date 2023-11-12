d0fdfa0fc9a7a79573c2c12e1ceea734ba90720e
Adev2598 <ammar.react.dev@outlook.com>
2023-11-12 13:21:01 +0400

    header and feature completed

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { HeaderSection, Features } from './components'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <HeaderSection/>
+      <Features/>
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ my-app/src/components/Features.jsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import '../Styles/FeatureStyle.css'
+import '../Styles/globalFeature.css'
+import '../Styles/featureGuide.css'
+
+const Features = () => {
+  return (
+    <React.Fragment>
+        <div class="feature-section">
+      <div className="features-that-make">Features That Make Selling<br />Easier</div>
+      <div className="frame">
+        <div className="div">
+          <div className="frame-2"></div>
+          <div className="frame-3">
+            <div className="frame-4">
+              <div className="frame-5">
+                <div className="text-wrapper">AI CRM</div>
+                <p className="book-jobs-faster">Book jobs faster with <br />artificial intelligence</p>
+              </div>
+              <p className="p">
+                Secure more jobs with AI-powered customer relationship management tools designed to help you grow.
+              </p>
+            </div>
+            <div className="frame-5">
+              <img className="image" src={"https://c.animaapp.com/Y6FbfLgQ/img/image-9@2x.png"} alt={'image'} loading='lazy'/>
+              <img className="img" src={"https://c.animaapp.com/Y6FbfLgQ/img/image-7@2x.png"} alt={'image'} loading='lazy'/>
+            </div>
+          </div>
+        </div>
+        <div className="frame-6">
+          <div className="frame-7">
+            <div className="frame-8"></div>
+            <div className="frame-9">
+              <div className="group">
+                <div className="overlap-group">
+                  <img className="image-2" src={"https://c.animaapp.com/Y6FbfLgQ/img/image-16@2x.png"} alt={'image'} loading='lazy'/>
+                  <div className="frame-10">
+                    <div className="ellipse"></div>
+                    <img className="vector" src={"https://c.animaapp.com/Y6FbfLgQ/img/vector.svg"} alt={'image'} loading='lazy'/>
+                    <img className="vector-2" src={"https://c.animaapp.com/Y6FbfLgQ/img/vector-1.svg"} alt={'image'} loading='lazy'/>
+                  </div>
+                </div>
+              </div>
+              <div className="frame-11">
+                <div className="frame-5">
+                  <div className="text-wrapper-2">Automated Replies</div>
+                  <p className="text-wrapper-3">Instantly follow up <br />with new customers</p>
+                </div>
+                <p className="create-an-automated">
+                  Create an automated response to any contact form <br />requests submitted to your Powerpro website
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="frame-12">
+            <div className="frame-13"></div>
+            <div className="frame-14">
+              <div className="overlap-wrapper">
+                <div className="overlap">
+                  <div className="overlap-2">
+                    <div className="image-wrapper">
+                      <img className="image-3" src={"https://c.animaapp.com/Y6FbfLgQ/img/image-19@2x.png"} alt={'image'} loading='lazy'/>
+                    </div>
+                    <div className="img-wrapper">
+                      <img className="image-4" src={"https://c.animaapp.com/Y6FbfLgQ/img/image-17@2x.png"} alt={'image'} loading='lazy'/>
+                    </div>
+                    <div className="overlap-group-wrapper">
+                      <div className="overlap-group-2">
+                        <img className="image-5" src={"https://c.animaapp.com/Y6FbfLgQ/img/image-15@2x.png"} alt={'image'} loading='lazy'/>
+                        <img className="image-6" src={"https://c.animaapp.com/Y6FbfLgQ/img/image-8@2x.png"} alt={'image'} loading='lazy'/>
+                        <img className="image-traced" src={"https://c.animaapp.com/Y6FbfLgQ/img/image-24--traced-.svg"} alt={'image'} loading='lazy'/>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="frame-15">
+                    <img className="image-7" src={"https://c.animaapp.com/Y6FbfLgQ/img/image-18@2x.png"} alt={'image'} loading='lazy'/>
+                  </div>
+                  <div className="frame-16">
+                    <img className="image-8" src={"https://c.animaapp.com/Y6FbfLgQ/img/image-20@2x.png"} alt={'image'} loading='lazy'/>
+                  </div>
+                  <div className="frame-17">
+                    <img className="image-9" src={"https://c.animaapp.com/Y6FbfLgQ/img/image-21@2x.png"} alt={'image'} loading='lazy'/>
+                  </div>
+                </div>
+              </div>
+              <div className="frame-11">
+                <div className="frame-5">
+                  <div className="text-wrapper-2">AI Generated Responess</div>
+                  <p className="text-wrapper-3">Send personalized <br />responses in one click</p>
+                </div>
+                <p className="sync-your-email-to">
+                  Sync your email to Powerpro and instantly generate <br />unique customer responses based on past
+                  messages.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </React.Fragment>
+  )
+}
+
+export default Features
